b20c31633999532a93eb0dcc690bd60d950360da
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-07-05 11:54:45 +0530

    added codepeb header

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Home = () => {
+    return (
+        <div className=''>
+            <Header/>
+            {/* Home */}
+        </div>
+    )
+}
+
+export default Home
