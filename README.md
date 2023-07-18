d5f42f11ff3ef4abd1e0012514675d556ad02ebe
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-07-18 12:01:44 +0530

    react-split plane issue clear

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-Git Repository For Codepen-Clone In 
--HTML
--CSS
--Git For Version Control
+## Codepen Code Editor Clone Using ReactJs
+
+TechStack
+- HTML
+- CSS
+- ReactJs
+- Material UI
+- Github (For Version Control)
+- Vercel (For Deployment)
+
+## Codepen-Clone
+* 
+
+Click on the Below Given Link to get the Preview Of The App
+
+<a href="https://sales-admin-dashboard.vercel.app/">
+<img src="https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white">
+</a>
+
+
+#### Home Page
+
+<img src="./images/admin_page1.png"/>
+
+<br>
+<img src="./images/admin_page2.png"/>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,15 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Code from './Code';
 import Result from './Result';
-import SplitPane, { Pane } from 'split-pane-react';
-import 'split-pane-react/esm/themes/default.css';
 
 const Home = () => {
-    const [sizes, setSizes] = useState([
-        // 100,
-        // '30%',
-        // 'auto',
-    ]);
 
-    const layoutCSS = {
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    };
     return (
         <div className=''>
-
-            <Header />
-            <Code />
-            <Result />
+                <Header />
+                <Code />
+                <Result />
         </div>
     )
 }
